4c0fd179255cdc4daece3ea144f4ed8e30a4ccff
JCloudYu <j.cloud.yu@gmail.com>
2017-10-27 19:11:32 +0800

    Allow module to keep multiple copyies of configurations

--- json-cfg.js
+++ json-cfg.js
@@ -4,45 +4,81 @@
 	
 	
 	
-	let __config = {};
-	let exports = module.exports = {};
+	let __instances = {};
+	let exports = module.exports = __GEN_CONFIG();
 	Object.defineProperties(exports, {
-		load:{
-			value:function(path){
-				let config = __LOAD_JSON_OBJECT(path);
-				
-				// Merge results
-				for( let idx in config ) {
-					if ( !config.hasOwnProperty(idx) ) {
-						continue;
-					}
-					
-					__config[idx] = config[idx];
+		obtain:{
+			value:(identifier)=>{
+				if ( !identifier ) {
+					throw "Config identifier is required!"
 				}
 				
+				if ( !__instances[identifier] ) {
+					__instances[identifier] = __GEN_CONFIG();
+				}
+				
+				return __instances[identifier];
+			},
+			writable:false, enumerable:false, configurable:true
+		},
+		remove:{
+			value:(identifier)=>{
+				if ( __instances[identifier] ) {
+					delete __instances[identifier];
+				}
+			
 				return exports;
 			},
-			writable:false, enumerable:true, configurable:false
-		},
-		save:{
-			value:function(path){
-				fs.writeFileSync(path, JSON.stringify(__config), {encoding:'utf8'});
-				return exports;
-			},
-			writable:false, enumerable:true, configurable:false
-		},
-		reset:{
-			value:function(){
-				__config = {};
-				return exports;
-			},
-			writable:false, enumerable:true, configurable:false
-		},
-		conf:{
-			get:()=>{ return __config; },
-			enumerable:true, configurable:false
+			writable:false, enumerable:false, configurable:true
 		}
 	});
+	
+	
+	
+	function __GEN_CONFIG(){
+		const confInst = {};
+		let __config = {};
+		
+		Object.defineProperties(confInst, {
+			load:{
+				value:function(path){
+					let config = __LOAD_JSON_OBJECT(path);
+					
+					// Merge results
+					for( let idx in config ) {
+						if ( !config.hasOwnProperty(idx) ) {
+							continue;
+						}
+						
+						__config[idx] = config[idx];
+					}
+					
+					return exports;
+				},
+				writable:false, enumerable:true, configurable:false
+			},
+			save:{
+				value:function(path){
+					fs.writeFileSync(path, JSON.stringify(__config), {encoding:'utf8'});
+					return exports;
+				},
+				writable:false, enumerable:true, configurable:false
+			},
+			reset:{
+				value:function(){
+					__config = {};
+					return exports;
+				},
+				writable:false, enumerable:true, configurable:false
+			},
+			conf:{
+				get:()=>{ return __config; },
+				enumerable:true, configurable:false
+			}
+		});
+	
+		return confInst;
+	}
 	
 	function __LOAD_JSON_OBJECT(path){
 		try {
--- test/test.js
+++ test/test.js
@@ -1,20 +1,22 @@
 (()=>{
 	"use strict";
 	
-	const config = require( '../js-cfg' );
-	
-	process.stdout.write( `Check loading...\n` );
+	const config = require( '../json-cfg' );
 	
 	{
-		process.stdout.write( `\tLoading... ` );
-		config.load( `${__dirname}/config-1.json` );
-		process.stdout.write( `${config.conf.name === "config1" ? 'pass' : 'fail'}\n` );
-	}
-	
-	{
-		process.stdout.write( `\tOverwriting... ` );
-		config.load( `${__dirname}/config-2.json` );
-		process.stdout.write( `${config.conf.name === "config2" ? 'pass' : 'fail'}\n` );
+		process.stdout.write( `Check loading...\n` );
+		
+		{
+			process.stdout.write( `\tLoading... ` );
+			config.load( `${__dirname}/config-1.json` );
+			process.stdout.write( `${config.conf.name === "config1" ? 'pass' : 'fail'}\n` );
+		}
+		
+		{
+			process.stdout.write( `\tOverwriting... ` );
+			config.load( `${__dirname}/config-2.json` );
+			process.stdout.write( `${config.conf.name === "config2" ? 'pass' : 'fail'}\n` );
+		}
 	}
 	
 	{
@@ -29,5 +31,15 @@
 		config.load( `${__dirname}/config-1.json` ).load( `${__dirname}/config-2.json` );
 		config.save( `${tmpDir}/config-new.json` ).reset().load( `${tmpDir}/config-new.json` );
 		process.stdout.write( `${config.conf.name === 'config2' ? 'pass' : 'fail'}\n` );
+	}
+	
+	{
+		process.stdout.write( `Check separation... ` );
+		config.reset().load( `${__dirname}/config-2.json` );
+		config.obtain( 'another' ).load( `${__dirname}/config-1.json` );
+		config.reset();
+		
+		let result = (Object.keys(config.conf).length === 0) && (Object.keys(config.obtain( 'another' ).conf).length !== 0);
+		process.stdout.write( `${result ? 'pass' : 'fail'}\n` );
 	}
 })();
